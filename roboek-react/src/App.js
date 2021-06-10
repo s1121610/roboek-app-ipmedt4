31a5138113a511aa1b45b010dee9c7d32ea3585c
Max <s1121610@student.hsleiden.nl>
2021-06-10 11:46:05 +0200

    Routing toegevoegd

--- roboek-react/src/App.js
+++ roboek-react/src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import './App.css';
 import Bibliotheek from "./Bibliotheek";
+import { Switch, Route, BrowserRouter as Router, Link } from "react-router-dom";
 
 //state = groep variabelen die van waarde kan veranderen.
 class App extends React.Component{    
     render() {
       return (
-        <Bibliotheek />
+        <Router>
+          <Switch>
+            <Route path="/bibliotheek">
+              <Bibliotheek />
+              <Link to="/">Terug</Link>
+            </Route>
+            <Route path="/">
+              <h1>Roboek</h1>
+              <h2>Menu:</h2>
+              <Link to="/bibliotheek">Bibliotheek</Link>
+            </Route>
+          </Switch>
+        </Router>
       )
     }
 }
