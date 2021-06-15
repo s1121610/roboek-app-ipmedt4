679d36c688718ca2afa69c535849e11a3a9831ea
Jasper Vonk <s1122268@student.hsleiden.nl>
2021-06-16 01:39:38 +0200

    Kickstart J2 Items overzicht

--- src/Boekenkast/Components/BoekenkastItems.js
+++ src/Boekenkast/Components/BoekenkastItems.js
@@ -11,21 +11,44 @@ class BoekenkastItems extends React.Component{
           <section className="boekenkastSection--top" style={{background: this.props.kast_kleur_primary}}>
             <article className="itemsSection__container">
               <div className="itemsSection__bookshelf" style={{background: this.props.kast_kleur_secondary}}>
-                <div className="itemsSection__shelfBook itemsSection__shelfBook--red"></div>
-                <div className="itemsSection__shelfBook itemsSection__shelfBook--green"></div>
-                <div className="itemsSection__shelfBook itemsSection__shelfBook--yellow"></div>
-                <div className="itemsSection__shelfBook itemsSection__shelfBook--purple itemsSection__shelfBook--tilted"></div>
-                <div className="itemsSection__shelfBook itemsSection__shelfBook--blue"></div>
+                <div className="itemsSection__shelfBook--red">
+                  <div className="itemsSection__shelfBook__Label--high"></div>
+                </div>
+                <div className="itemsSection__shelfBook--green">
+                  <div className="itemsSection__shelfBook__Label--low"></div>
+                </div>
+                <div className="itemsSection__shelfBook--yellow">
+                  <div className="itemsSection__shelfBook__Label--high"></div>
+                </div>
+                <div className="itemsSection__shelfBook--purple itemsSection__shelfBook--tilted">
+                  <div className="itemsSection__shelfBook__Label--high"></div>
+                  <div className="itemsSection__shelfBook__Label--low"></div>
+                </div>
+                <div className="itemsSection__shelfBook--blue">
+                  <div className="itemsSection__shelfBook__Label--high"></div>
+                </div>
               </div>
               <div className="itemsSection__itemshelf" style={{background: this.props.kast_kleur_secondary}}>
-                <div className="itemsSection__itemslot"></div>
-                <div className="itemsSection__itemslot"></div>
-                <div className="itemsSection__itemslot"></div>
+                <figure className="itemsSection__itemslot">
+                  <img className="itemsSection__itemslot__image" src="img/default_item_kastdecoratie.png" alt="INVULLEN" />
+                </figure>
+                <figure className="itemsSection__itemslot">
+                  <img className="itemsSection__itemslot__image" src="img/default_item_kastdecoratie.png" alt="INVULLEN" />
+                </figure>
+                <figure className="itemsSection__itemslot">
+                  <img className="itemsSection__itemslot__image" src="img/default_item_kastdecoratie.png" alt="INVULLEN" />
+                </figure>
               </div>
               <div className="itemsSection__itemshelf" style={{background: this.props.kast_kleur_secondary}}>
-                <div className="itemsSection__itemslot"></div>
-                <div className="itemsSection__itemslot"></div>
-                <div className="itemsSection__itemslot"></div>
+                <figure className="itemsSection__itemslot">
+                  <img className="itemsSection__itemslot__image" src="img/default_item_kastdecoratie.png" alt="INVULLEN" />
+                </figure>
+                <figure className="itemsSection__itemslot">
+                  <img className="itemsSection__itemslot__image" src="img/default_item_kastdecoratie.png" alt="INVULLEN" />
+                </figure>
+                <figure className="itemsSection__itemslot">
+                  <img className="itemsSection__itemslot__image" src="img/default_item_kastdecoratie.png" alt="INVULLEN" />
+                </figure>
               </div>
             </article>
           </section>
