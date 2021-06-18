ef1e0feb1220206b853f611269e6f4e3ac4af11b
Daniel van Droffelaar <daniel@realafford.nl>
2021-06-18 14:30:08 +0200

    Is weer werkend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
-=======
 
->>>>>>> Development
 import './App.css';
 import Genre from "./Bibliotheek/Genre";
 import Bibliotheek from "./Bibliotheek/Bibliotheek";
 import Details from "./Bibliotheek/Details";
-<<<<<<< HEAD
 import Woordzoeker from "./Woordzoeker/Woordzoeker"
-=======
 import Boekenkast from "./Boekenkast/Boekenkast";
->>>>>>> Development
 
 import { Switch, Route, BrowserRouter as Router, Link } from "react-router-dom";
 
@@ -33,23 +27,18 @@ class App extends React.Component{
               <Link to="/genre">Terug</Link>
               <Details />
             </Route>
-<<<<<<< HEAD
             <Route path="/woordzoeker">
               <Woordzoeker />
-=======
+            </Route>
             <Route path="/boekenkast">
               <Link to="/">Terug</Link>
               <Boekenkast user_id='1'/>
->>>>>>> Development
             </Route>
             <Route path="/">
               <h1>Roboek</h1>
               <h2>Menu:</h2>
               <Link to="/genre">Bibliotheek</Link>
-<<<<<<< HEAD
-=======
               <Link to="/boekenkast">Boekenkast</Link>
->>>>>>> Development
             </Route>
           </Switch>
         </Router>
