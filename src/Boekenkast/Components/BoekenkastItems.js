88e4860866a5552a7cbf553c7ec9477e0c6f4986
Jasper Vonk <s1122268@student.hsleiden.nl>
2021-06-18 02:44:11 +0200

    Kickstart J3 Medailles overzicht
    - BoekenkastMedailles; Styling, medailles inladen & tonen

--- src/Boekenkast/Components/BoekenkastItems.js
+++ src/Boekenkast/Components/BoekenkastItems.js
@@ -10,31 +10,33 @@ class BoekenkastItems extends React.Component{
         <article className="boekenkastZijde">
           <section className="boekenkastSection--top" style={{background: this.props.kast_kleur_primary}}>
             <article className="itemsSection__container">
-              <div className="itemsSection__bookshelf" style={{background: this.props.kast_kleur_secondary}}>
-                <div className="itemSection__shelfBook itemsSection__shelfBook--red">
+              <ul className="itemsSection__bookshelf" style={{background: this.props.kast_kleur_secondary}}>
+                <li className="itemSection__shelfBook itemsSection__shelfBook--red">
                   <div className="itemsSection__shelfBook__Label--high"></div>
-                </div>
-                <div className="itemSection__shelfBook itemsSection__shelfBook--green">
+                </li>
+                <li className="itemSection__shelfBook itemsSection__shelfBook--green">
                   <div className="itemsSection__shelfBook__Label--low"></div>
-                </div>
-                <div className="itemSection__shelfBook itemsSection__shelfBook--yellow">
+                </li>
+                <li className="itemSection__shelfBook itemsSection__shelfBook--yellow">
                   <div className="itemsSection__shelfBook__Label--high"></div>
-                </div>
-                <div className="itemSection__shelfBook itemsSection__shelfBook--purple itemsSection__shelfBook--tilted">
+                </li>
+                <li className="itemSection__shelfBook itemsSection__shelfBook--purple itemsSection__shelfBook--tilted">
                   <div className="itemsSection__shelfBook__Label--high"></div>
                   <div className="itemsSection__shelfBook__Label--low"></div>
-                </div>
-                <div className="itemSection__shelfBook itemsSection__shelfBook--blue">
+                </li>
+                <li className="itemSection__shelfBook itemsSection__shelfBook--blue">
                   <div className="itemsSection__shelfBook__Label--high"></div>
-                </div>
-              </div>
-              <div className="itemsSection__itemshelf" style={{background: this.props.kast_kleur_secondary}}>
+                </li>
+              </ul>
+              <ul className="itemsSection__itemshelf" style={{background: this.props.kast_kleur_secondary}}>
                 {this.props.items.map((item, index) =>
-                  <figure key={item.id} className="itemsSection__itemslot">
-                    <img className="itemsSection__itemslot__image" src={item.image} alt={item.naam} />
-                  </figure>
+                  <li key={item.id}>
+                    <figure className="itemsSection__itemslot">
+                      <img className="itemsSection__itemslot__image" src={item.image} alt={item.naam} />
+                    </figure>
+                  </li>
                 )}
-              </div>
+              </ul>
             </article>
           </section>
           <section className="boekenkastSection--bottom" style={{background: this.props.kast_kleur_primary}}>
--- src/Boekenkast/Components/BoekenkastMedailles.js
+++ src/Boekenkast/Components/BoekenkastMedailles.js
@@ -9,6 +9,18 @@ class BoekenkastMedailles extends React.Component{
       <React.Fragment>
         <article className="boekenkastZijde">
           <section className="boekenkastSection--top" style={{background: this.props.kast_kleur_primary}}>
+            <article className="medaillesSection">
+              <h1 className="medaillesSection__header"> Medailles </h1>
+              <ul className="medaillesSection__list">
+                {this.props.medailles.map((medaille, index) =>
+                  <li key={medaille.id} className="medaillesSection__listItem">
+                    <figure className="medaillesSection__medailleslot">
+                      <img className="medaillesSection__medailleslot__image" src={medaille.image} alt={medaille.naam + " Medaille"} />
+                    </figure>
+                  </li>
+                )}
+              </ul>
+            </article>
           </section>
           <section className="boekenkastSection--bottom" style={{background: this.props.kast_kleur_primary}}>
           </section>
