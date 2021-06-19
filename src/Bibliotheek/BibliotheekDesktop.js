5a74388cdc88c986787becc5a6fc773145af4fef
Max <s1121610@student.hsleiden.nl>
2021-06-19 11:50:47 +0200

    Screensize wordt gecheckt in bibliotheek.js

---
+++ src/Bibliotheek/BibliotheekDesktop.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Genre.css';
+import './Bibliotheek.css';
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+class BibliotheekDesktop extends React.Component{
+    state = {
+        genres: []
+    }
+
+    render(){
+        return(
+            <main>
+                <h2>Netflix voor boeken</h2>
+                
+            </main>
+        );
+    }
+}
+
+export default BibliotheekDesktop;
--- src/Bibliotheek/Genre.js
+++ src/Bibliotheek/Genre.js
@@ -8,7 +8,6 @@ class Genre extends React.Component{
     state = {
         genres: []
     }
-
     componentDidMount() {
         axios.get(`http://127.0.0.1:8000/api/bibliotheek/choose/genre`)
           .then(res => {
@@ -18,17 +17,22 @@ class Genre extends React.Component{
       }
 
     render(){
-        return(
-            <main>
-                <h2>Kies een genre</h2>
-                <ul>
-                    {this.state.genres.map(genre => <li className="genre">
-                        <Link className="genre__naam" data-genre={genre.naam} to={'/bibliotheek/' + genre.naam}>{genre.naam}</Link>
-                    </li>)}
-                    <li className="genre"><Link className="genre__naam" data-genre="Alle_boeken" to={'/bibliotheek/'}>Alle boeken</Link></li>
-                </ul>
-            </main>
-        );
+        if(window.innerWidth <= 900){
+            return(
+                <main>
+                    <h2>Kies een genre</h2>
+                    <ul>
+                        {this.state.genres.map(genre => <li className="genre">
+                            <Link className="genre__naam" data-genre={genre.naam} to={'/bibliotheek/' + genre.naam}>{genre.naam}</Link>
+                        </li>)}
+                        <li className="genre"><Link className="genre__naam" data-genre="Alle_boeken" to={'/bibliotheek/'}>Alle boeken</Link></li>
+                    </ul>
+                </main>
+            );
+        }else{
+            
+        }
+        
     }
 }
 
