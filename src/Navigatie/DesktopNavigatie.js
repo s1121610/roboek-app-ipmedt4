91dbf66c6cc5b6caa9f775b3bb03ebe3f08bd851
Timo van den Berg <74822698+TimovandenBerg@users.noreply.github.com>
2021-06-21 15:44:57 +0200

    Desktop navigatie werkt nu

--- src/Navigatie/DesktopNavigatie.js
+++ src/Navigatie/DesktopNavigatie.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import DesktopNavItem from './DesktopNavItem';
+
+import './DesktopNavigatie.css'
+
 class DesktopNav extends React.Component {
     render() {
         return (
-            <section>
-                <h2>DesktopNav</h2>
+            <section className="desktopNav">
+                <DesktopNavItem link="Boekenlijst" p="Boekenlijst"/>
+                <DesktopNavItem link="Bibliotheek" p="Bibliotheek" />
+                <DesktopNavItem link="Boekenkast" p="Boekenkast" />
+                <DesktopNavItem link="Winkel" p="Winkel" />
+                <section className="desktopNav__hulp">
+                    <p className="desktopNav__hulp__p">Hulp</p>
+                </section>
             </section>
         );
     }
